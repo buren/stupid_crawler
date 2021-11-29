255b7a2cf57222cbd4c8b0c90aa59c26bdde1465
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-30 00:14:24 +0800

    Delete lib directory

--- lib/stupid_crawler.rb
+++
@@ -1,6 +0,0 @@
-require 'stupid_crawler/version'
-require 'stupid_crawler/crawler'
-require 'stupid_crawler/result_writer'
-
-module StupidCrawler
-end
--- lib/stupid_crawler/version.rb
+++
@@ -1,3 +0,0 @@
-module StupidCrawler
-  VERSION = '0.2.1'
-end
